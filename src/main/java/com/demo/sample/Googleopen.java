eba2560dbb40eedca348948a6f46caaf1502589a
arvindhs <arvindbond52@gmail.com>
2025-09-25 20:04:42 +0530

    first commit

---
+++ src/main/java/com/demo/sample/Googleopen.java
@@ -0,0 +1,22 @@
+package com.demo.sample;
+
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+// import org.openqa.selenium.Keys;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Googleopen {
+    public static void main(String[] args) {
+
+        ChromeDriver driver = new ChromeDriver();
+        driver.get("https://arvindhsonline.vercel.app");
+        driver.findElement(By.name("user_name")).sendKeys("arvind");
+        driver.findElement(By.name("user_email")).sendKeys("sample@email.com");
+        driver.findElement(By.name("message")).sendKeys("Hi this the sample mail for the testing in the selinum by the java code");
+        driver.findElement(By.xpath("//button[@type = 'submit']")).click();
+        Alert alert = driver.switchTo().alert();
+        alert.accept();
+        driver.close();
+
+    }
+}
